6647a75eb84ab96ced8ada417da91f8a1498b901
Audy <muhamadaudy14@gmail.com>
2025-09-28 13:18:44 +0700

    06 Repeat string

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
 
+const repeatString = function (string, number) {
+    let result = "";
+    for (let i = 0; i < number; i++) {
+        result = result + string;
+    } if (number < 0) {
+        return "ERROR";
+    }
+    return result;
 };
-
+// repeatString('hey', 3)
 // Do not edit below this line
 module.exports = repeatString;
