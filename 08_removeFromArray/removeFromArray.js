96bd5eca837285a5f18aaf02f0e644cd0dbdf383
Audy <muhamadaudy14@gmail.com>
2025-09-28 17:48:03 +0700

    08 Remove from array

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...argument) {
+    return arr.filter(item => !argument.includes(item));
+
 };
 
 // Do not edit below this line
