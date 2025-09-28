bb8a6a0b62356a83e63a8bcba4f99714d6195be1
Audy <muhamadaudy14@gmail.com>
2025-09-28 17:29:59 +0700

    07 Reverse string

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+// × reverses single word (5 ms)
+// √ reverses multiple words (1 ms)
+// × works with numbers and punctuation (2 ms)
+// × works with blank strings (1 ms) 
+const reverseString = function (greeting) {
+    let reverse = "";
+    for (let i = greeting.length - 1; i >= 0; i--) {
+        reverse = reverse + greeting[i]
 
+    }
+    return reverse;
 };
 
 // Do not edit below this line
